3e7a98e7fbf94d2a1907f73663fca00ed157b334
NadiaQN <quezada.nadia@gmail.com>
2018-03-22 10:05:57 -0300

    agregando dependencias, scripts, etc

--- src/Components/Dashboard/Menu/Menu.js
+++ src/Components/Dashboard/Menu/Menu.js
@@ -1,1 +1,18 @@
 import React from 'react';
+import { Switch, Route } from "react-router-dom";
+import Catalogo from './Catalogo/Catalogo';
+import Informes from './Informes/Informes';
+import Transacciones from './Transacciones/Transacciones';
+import Registros from './Registros/Registros';
+
+
+const Menu = () => (
+    <menu>
+            <Route exact path="/" component={Catalogo} />
+            <Route path="/inventory" component={Transacciones} />
+            <Route path="/transactions" component={Registros} />
+            <Route path="/livecart" component={Informes} />
+    </menu>
+);
+
+export default Menu;
